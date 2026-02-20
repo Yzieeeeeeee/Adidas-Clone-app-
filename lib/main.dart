57a6db9893f42f73373edbce5ca382eb78a7c31d
Muhammed yasir <my7471302@gmail.com>
2026-02-21 01:53:02 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,11 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   var firebaseOptions = const FirebaseOptions(
-    apiKey: "AIzaSyCFQLLL8a_4sqqFZMuCOW41E7IJfPH4v4M",
-    appId: "1:856276416659:android:c8ea9b9f9cfd200ac43566",
-    messagingSenderId: "856276416659",
-    projectId: "whatsapp-dd9b9",
-    storageBucket: "whatsapp-dd9b9.appspot.com",
+    apiKey: "",
+    appId: "",
+    messagingSenderId: "",
+    projectId: "",
+    storageBucket: "",
   );
 
   await Firebase.initializeApp(options: firebaseOptions);
